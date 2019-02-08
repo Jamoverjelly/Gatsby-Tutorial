410f9915110dd1676f1555d56bb1448812eef239
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-02-08 16:02:34 -0500

    Working with routing using Gatsby's <Link/> component.

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <h1>Hello Jamie!</h1>
-    <p>What a world!</p>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello Jamie!"/>
+    <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt=""/>
   </div>
 )
