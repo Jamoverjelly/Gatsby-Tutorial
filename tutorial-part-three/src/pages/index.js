51ce0a870438c002f109782d5bc00460bbdfd4db
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-02-09 14:19:03 -0500

    Fixed nesting of p element in index.js

--- tutorial-part-three/src/pages/index.js
+++ tutorial-part-three/src/pages/index.js
@@ -4,7 +4,9 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>What do I like to do? Lots of course, but definitely enjoy building
-      websites.</p>
+    <p>
+      What do I like to do? Lots of course, but definitely enjoy building
+      websites.
+    </p>
   </Layout>
 )
