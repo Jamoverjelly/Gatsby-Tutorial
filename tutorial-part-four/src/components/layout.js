b23f380d43132a5571fea6b875dfc0a0229648d4
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-02-11 17:09:19 -0500

    Tutorial part five: Added plugin for fetching data from the filesystem, gatsby-source-filesystem

--- tutorial-part-four/src/components/layout.js
+++ tutorial-part-four/src/components/layout.js
@@ -17,36 +17,35 @@ export default ({ children }) => (
      `
     }
       render={data => (
-
-      <div
-        css={css`
-        margin: 0 auto,
-        max-width: 700px;
-        padding: ${rhythm(2)};
-        padding-top: ${rhythm(1.5)}
-        `}
-      >
-        <Link to={`/`}>
-          <h3
-            css={css`
-            margin-bottom: ${rhythm(2)};
-            display: inline-block;
-            font-style: normal;
-            `}
-          >
-            {data.site.siteMetadata.title}
-          </h3>
-        </Link>
-        <Link
-          to={`/about/`}
+        <div
           css={css`
-          float: right;    
+          margin: 0 auto,
+          max-width: 700px;
+          padding: ${rhythm(2)};
+          padding-top: ${rhythm(1.5)}
           `}
-          >
-          About
-        </Link>
-        {children}
-      </div>
+        >
+          <Link to={`/`}>
+            <h3
+              css={css`
+              margin-bottom: ${rhythm(2)};
+              display: inline-block;
+              font-style: normal;
+              `}
+            >
+              {data.site.siteMetadata.title}
+            </h3>
+          </Link>
+          <Link
+            to={`/about/`}
+            css={css`
+            float: right;    
+            `}
+            >
+            About
+          </Link>
+          {children}
+        </div>
     )}
   />  
 )
